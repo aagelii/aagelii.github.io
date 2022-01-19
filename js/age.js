d12c82e08dc23bd65ca3ad963956f74575d9ce81
axel agelii <aagelii17@gmail.com>
2022-01-18 19:48:51 -0600

    initialization

--- js/age.js
+++ js/age.js
@@ -1,6 +1,6 @@
 let ageEl = document.getElementById("age");
 
 setInterval(() => {
-	let time = (new Date() - new Date(1011326400)) / (1000 * 60 * 60 * 24 * 365.25); // milliseconds per year
+	let time = (new Date() - new Date('January 17, 2002 22:00:00')) / (1000 * 60 * 60 * 24 * 365.25); // milliseconds per year
 	ageEl.innerText = time.toString().substring(0, 12);
 }, 50);
