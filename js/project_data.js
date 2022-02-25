d3451efd1071dfb79c5ebb88f3cac40bb0db0a12
aagelii <aagelii17@gmail.com>
2022-02-24 23:32:20 -0600

    finished

--- js/project_data.js
+++ js/project_data.js
@@ -1,33 +1,33 @@
 const ProjectData = {
 	"moviemapper": {
 		title: "Movie Mapper (Data Wrangler)",
-		github: "",
-		description: "This application will allow users to explore a movie data set, and find new movies to watch. The data set contains short descriptions, user rating averages, one or more genres, and additional metadata (including title, actors, and director) for each of the movies. Users will be able to select search criteria which consist of one or more genres to select movies by and a range of ratings. Users will be able to select any set of genres, and rating ranges. The system will then display the number of movies that fit the search criteria and the top three of these movies (by rating) with additional details. Users will be able to scroll through the entire list of movies or adapt the search criteria at any time with the app. Primary users of the app will be people who want to find new and interesting movies to watch. "
+		github: "https://github.com/aagelii/cs400moviemapper",
+		description: "For this project, I was the data wrangler, meaning the code I wrote reads the data from the csv file and transforms it into usable data. This application will allow users to explore a movie data set, and find new movies to watch. The data set contains short descriptions, user rating averages, one or more genres, and additional metadata (including title, actors, and director) for each of the movies. Users will be able to select search criteria which consist of one or more genres to select movies by and a range of ratings. Users will be able to select any set of genres, and rating ranges. The system will then display the number of movies that fit the search criteria and the top three of these movies (by rating) with additional details. Users will be able to scroll through the entire list of movies or adapt the search criteria at any time with the app. Primary users of the app will be people who want to find new and interesting movies to watch. "
 	},
 	"hurricanemapper": {
 		title: "Hurricane Mapper (Integration Manager)",
-		github: "",
-		description: "This application will allow users to explore and add data to a hurricane data set. The data set contains information on the formation and dissipation dates of the hurricane, death count, and the hurricanes’ names and speeds. The data comes from a CSV file (hurricanes.csv) that was made by Professor Meena Syamkumar and used in CS220. All of this data will be populated into a red-black tree at the start of the program. The people using an application like would be anyone interested in hurricanes, ie. meteorologist."
+		github: "https://github.com/aagelii/cs400hurricanemapper",
+		description: "For this project, I was the integration manager, I took all the parts (data wrangler, frontend, and backend) and essentially connected them together. This application will allow users to explore and add data to a hurricane data set. The data set contains information on the formation and dissipation dates of the hurricane, death count, and the hurricanes’ names and speeds. The data comes from a CSV file (hurricanes.csv) that was made by Professor Meena Syamkumar and used in CS220. All of this data will be populated into a red-black tree at the start of the program. The people using an application like would be anyone interested in hurricanes, ie. meteorologist."
 	},
 	"planemapper": {
 		title: "Plane Mapper (Frontend Developer)",
-		github: "",
-		description: "The app we are developing will map plane and airport locations to a graph and allow the user to find different routes based on the shortest or least costly route. Someone who would use it would be a traveler looking to travel as efficiently and quickly as possible and find the shortest route."
+		github: "https://github.com/aagelii/cs400planemapper",
+		description: "For this project, I was the frontend developer, the code that I wrote was for the interface used by the user. The app we are developing will map plane and airport locations to a graph and allow the user to find different routes based on the shortest or least costly route. Someone who would use it would be a traveler looking to travel as efficiently and quickly as possible and find the shortest route."
 	},
 	"MNSIT": {
 		title: "MNSIT",
-		github: "",
-		description: "Is this working?"
+		github: "https://github.com/aagelii/cs540hw5",
+		description: "The classic <a href=\"http://yann.lecun.com/exdb/mnist/\">MNIST</a> machine learning project. Training a neural network to classify handwritten digits."
 	},
 	"RL": {
 		title: "Reinforcement Learning",
-		github: "",
+		github: "https://github.com/aagelii/reinforcementLearningTradingBot",
 		description: "Recently over winter break, I looked into making a trading bot that used reinforcement learning. For this project I used OpenAI Gym gym-Anytrading for RL trading, trained a trading bot with RL using Stable Baselines, and used Marketwatch trading data of different companies for training."
 	},
 	"ML": {
-		title: "Machine Learning",
-		github: "",
-		description: "Hey does this work?"
+		title: "Artificial Intelligence/Machine Learning",
+		github: "https://github.com/aagelii/cs540hw",
+		description: "This github takes you to my coursework for CS540-- Intro to AI, instructor: <a href=\"https://pages.cs.wisc.edu/~yliang/cs540_1_fall21/index.html\">Yingyu Liang</a> <br> This is the area that I'm most interested in Computer Science. I would love to get involved in more projects or research!"
 	},
 	"cs577": {
 		title: "CS577-- Intro to Algorithms",
@@ -47,6 +47,6 @@ const ProjectData = {
 	"portfolio": {
 		title: "Portfolio",
 		github: "https://github.com/aagelii/aagelii.github.io",
-		description: "You're looking at it, made it with HTML/CSS/JS",
+		description: "You're looking at it, this website was made with HTML/CSS/JS",
 	},
 };
